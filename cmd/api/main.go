ac1bfd5dfe715de7fa627d180d0bd2ab895ff331
matheus griza <matheusgriza@sou.faccat.br>
2025-11-17 17:32:40 -0300

    first commit

---
+++ cmd/api/main.go
@@ -0,0 +1,26 @@
+package api
+
+import "fmt"
+
+/*
+Sistema de Agenda
+
+- Armazenar para cada compromisso:
+	1 - Data/Horário
+	2 - Titulo do compromisso
+	3 - Descrição do compromisso (opcional)
+	4 - Pessoas Envolvidas
+
+	- As pessoas devem ser armazenadas individualmente
+
+-Funcionalidades:
+	1 - Listar Compromissos
+	2 - Cadastrar Compromisso
+	4 - Alterar titulo e descriçao em um compromisso
+	5 - Exlcuir uma pessoa em um compromisso
+	6 - Excluir um comprmisso
+*/
+
+func main() {
+	fmt.Println("Hola mundo")
+}
